ccdece096257074ab222e8e0e1d5b2cb11f581ed
GarfDev <garfdev.13@gmail.com>
2020-02-27 04:17:33 +0700

    Updated webpack devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
-/* eslint-disable import/no-extraneous-dependencies */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
+  devServer: {
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: false,
+      errorDetails: false,
+      warnings: false,
+      publicPath: false
+    },  
   entry: './app/app',
   output: {
     path: path.join(__dirname, '/build'),
