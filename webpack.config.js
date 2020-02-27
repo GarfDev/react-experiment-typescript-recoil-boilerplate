9645b23c792faa37832d183c7073b37ebb08afb8
GarfDev <garfdev.13@gmail.com>
2020-02-27 09:19:25 +0700

    TSLint awesome af

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devServer: {
-    stats: 'minimal',
+    port: 3000,
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: false,
+      errorDetails: false,
+      warnings: false,
+      publicPath: false,
+    },
   },
   entry: './app/app',
   output: {
