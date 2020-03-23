de629662c5249fa6053515ebfd996392c1c077f3
GarfDev <garfdev.13@gmail.com>
2020-03-23 15:18:33 +0700

    Removed initial Data loading due to conflict with netlify .env

--- app/modules/App/index.tsx
+++ app/modules/App/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import RootRouter from './route';
-import { verifyPreviousSession } from '../@core/Session/actions';
 
 export default function App() {
-  const dispatch = useDispatch();
-  React.useEffect(() => {
-    dispatch(verifyPreviousSession());
-  }, [dispatch]);
-
   return <RootRouter />;
 }
